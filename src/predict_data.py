4a5b0e48b5df6424ddf61ba34d568c0d09e68335
baibai25 <kamuy.camel.0225@gmail.com>
2019-04-15 19:30:51 +0900

    Update

--- src/predict_data.py
+++ src/predict_data.py
@@ -1,14 +1,19 @@
 import re
 import pandas as pd
 from imblearn import metrics
+from sklearn.metrics import f1_score
+from sklearn.metrics import matthews_corrcoef
 
 # calc evaluate metrics
 def calc_metrics(y_test, pred, auc, i):
         sen = metrics.sensitivity_score(y_test, pred, pos_label=1)   
         spe = metrics.specificity_score(y_test, pred, pos_label=1)   
-        geo = metrics.geometric_mean_score(y_test, pred, pos_label=1)   
+        geo = metrics.geometric_mean_score(y_test, pred, pos_label=1)
+        f1 = f1_score(y_test, pred, pos_label=1)
+        mcc = matthews_corrcoef(y_test, pred)
+          
         index = ['sm', 'b1', 'b2', 'enn', 'tom', 'ada', 'mnd'] 
-        metrics_list = [index[i], sen, spe, geo, auc]
+        metrics_list = [index[i], sen, spe, geo, f1, mcc, auc]
         return metrics_list
 
 # convert classification report to dataframe
