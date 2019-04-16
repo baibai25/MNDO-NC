507c0504380f3e9d8df4a8e314f5420bb82a3e77
baibai25 <kamuy.camel.0225@gmail.com>
2019-04-16 17:31:06 +0900

    Update for keel datasets

--- over-sampling.py
+++ over-sampling.py
@@ -7,6 +7,13 @@ from src import multivariate_os, nominal_os
 from collections import Counter
 from sklearn.model_selection import train_test_split
 
+# set save path
+def set_path(basename):
+    os.makedirs('./generated', exist_ok=True)
+    name = os.path.splitext(basename)
+    save_path = 'generated/{}.csv'.format(name[0]) 
+    return save_path, name[0]
+
 # Multivariate over-sampling
 def mndo(pos, num_minority):
     pos, zero_std = multivariate_os.find_zerostd(pos, num_minority)
@@ -19,16 +26,22 @@ if __name__ == '__main__':
 
     # Load dataset
     try:
-        #data = pd.read_csv('Predataset/abalone_19/continuous.csv')
-        #nominal = pd.read_csv('Predataset/abalone_19/nominal.csv')
-        data = pd.read_csv('Predataset/{}/continuous.csv'.format(sys.argv[1]))
-        nominal = pd.read_csv('Predataset/{}/nominal.csv'.format(sys.argv[1]))
-        file_name = sys.argv[1]
+        data = pd.read_csv(sys.argv[1])
+        save_path, file_name = set_path(os.path.basename(sys.argv[1]))
+        #data = pd.read_csv('Predataset/{}/continuous.csv'.format(sys.argv[1]))
+        #nominal = pd.read_csv('Predataset/{}/nominal.csv'.format(sys.argv[1]))
     except IndexError:
         sys.exit('error: Must specify dataset file')
     except FileNotFoundError:
         sys.exit('error: No such file or directory')
-         
+    
+    # split nominal data
+    categorical = [data.columns[i] for i in range(data.shape[1]) if data.dtypes[i] == 'object']
+    nominal = data[categorical]
+    nominal = pd.get_dummies(nominal)
+    nominal = pd.concat([nominal, data['Label']], axis=1)
+    data = data.drop(categorical, axis=1)
+
     # split the data
     X = data.drop('Label', axis=1)
     y = data.Label
@@ -43,11 +56,11 @@ if __name__ == '__main__':
     nominal = nominal.drop('Label', axis=1)
 
     # calc number of  samples to synthesize
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4,
             shuffle=True, random_state=42)
 
     cnt = Counter(y_train)
-    num_minority = int((cnt[-1] - cnt[1]))
+    num_minority = int((cnt[0] - cnt[1]))
     
     # over-sampling
     pos_gen = mndo(pos, num_minority)
@@ -55,5 +68,5 @@ if __name__ == '__main__':
     # nominal over-sampling
     key = nominal_os.distance(positive, pos_gen)
     generated_data = nominal_os.nominal_os(key, nominal, pos_gen)
-    nominal_os.save(generated_data, file_name)
+    nominal_os.save(generated_data, save_path)
 
